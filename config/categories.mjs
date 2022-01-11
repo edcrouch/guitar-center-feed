ca7b6138ec9dfe9282939dc3b1cdfb7cb2a61cc6
Edward Crouch <edwardascrouch@gmail.com>
2022-01-11 12:47:51 -0600

    Add modulation category

--- config/categories.mjs
+++ config/categories.mjs
@@ -3,6 +3,7 @@ export default Object.freeze({
   distortionAndOverdrive: '/Used/Distortion-Overdrive-Effects-Pedals.gc',
   delayAndReverb: '/Used/Delay-Reverb-Effects-Pedals.gc',
   equalizer: '/Used/Equalizer-Effects-Pedals.gc',
+  chorusFlangerAndPhaser: '/Used/Chorus-Flanger-Phaser-Effects-Pedals.gc',
 
   guitarAmplifiers: '/Used/Guitar-Amplifiers.gc',
   GuitarAmplifierCombos: '/Used/Combo-Guitar-Amplifiers.gc',
