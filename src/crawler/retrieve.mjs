29722b563271d3aaecb221335f5ef0c26f74372e
Edward Crouch <edwardascrouch@gmail.com>
2022-02-04 21:04:24 -0600

    Refactor logic using loop instead of recursion (memory footprint)

--- src/crawler/retrieve.mjs
+++ src/crawler/retrieve.mjs
@@ -6,39 +6,45 @@ const baseURL = 'https://www.guitarcenter.com'
 const allowedChars = /[^a-z0-9+]/gi
 
 export async function getResults(config) {
-  return doResults(`${baseURL}${config.category}`, config.searchTerms)
+  return doResultsLoop(`${baseURL}${config.category}`, config.searchTerms)
 }
 
 async function doResults(url, terms) {
-  const resp = await axios.get(url)
-  const { document } = (new JSDOM(resp.data)).window
-  const matches = Array.from(document.querySelectorAll('div.product')).filter(el => findMatch(el.querySelector('div.productTitle > a'), terms))
-  const next = document.querySelector('a.page-link.-next')
+  let next = 1
 
-  const matchJson = matches.map(el => {
-    const link = el.querySelector('div.productTitle > a')
-    return {
-      imageLink: el.querySelector('img[data-original]').attributes.getNamedItem('data-original').value,
-      title: link.innerHTML,
-      productLink: baseURL + link.href,
-      price: el.querySelector('span.productPrice').innerHTML.match(/[\d,]*\.\d{2}$/)[0].replace(',', ''),
-      condition: el.querySelector('div.productCondition').innerHTML,
-      itemId: el.querySelector('var.productId').innerHTML
-    }
-  })
-
-  if (next) {
-    const results = await doResults(`${baseURL}${next.href}`, terms)
-    return {
-      matches: matchJson.concat(results.matches),
-      itemIds: results.itemIds.concat(Array.from(document.querySelectorAll('var.productId')).map(el => el.innerHTML))
-    }
+  let items = {
+    matches: [],
+    itemIds: []
   }
 
-  return {
-    matches: matchJson,
-    itemIds: Array.from(document.querySelectorAll('var.productId')).map(el => el.innerHTML)
-  }
+  do {
+    const resp = await axios.get(url)
+    const document = JSDOM.fragment(resp.data)
+    const matches = Array.from(document.querySelectorAll('div.product'))
+      .filter(el => findMatch(el.querySelector('div.productTitle > a'), terms))
+
+    next = document.querySelector('a.page-link.-next')
+
+    items.matches = items.matches.concat(matches.map(el => {
+      const link = el.querySelector('div.productTitle > a')
+      return {
+        imageLink: el.querySelector('img[data-original]').attributes.getNamedItem('data-original').value,
+        title: link.innerHTML,
+        productLink: baseURL + link.href,
+        price: el.querySelector('span.productPrice').innerHTML.match(/[\d,]*\.\d{2}$/)[0].replace(',', ''),
+        condition: el.querySelector('div.productCondition').innerHTML,
+        itemId: el.querySelector('var.productId').innerHTML
+      }
+    }))
+
+    items.itemIds = items.itemIds.concat(Array.from(document.querySelectorAll('var.productId')).map(el => el.innerHTML))
+
+    if (next) {
+      url = `${baseURL}${next.href}`
+    }
+  } while (next)
+
+  return items
 }
 
 function findMatch(el, terms) {
