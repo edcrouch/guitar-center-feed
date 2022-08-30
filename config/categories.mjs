fcc7eaf0bcfd6926e6410690b4317cabc29da050
Edward Crouch <edwardascrouch@gmail.com>
2022-08-29 19:01:18 -0500

    Add more categories

--- config/categories.mjs
+++ config/categories.mjs
@@ -1,11 +1,18 @@
 export default Object.freeze({
   effectsPedals: '/Used/Effects-Pedals.gc',
   distortionAndOverdrive: '/Used/Distortion-Overdrive-Effects-Pedals.gc',
+  fuzzAndBoost: '/Used/Fuzz-Boost-Effects-Pedals.gc',
   delayAndReverb: '/Used/Delay-Reverb-Effects-Pedals.gc',
   equalizer: '/Used/Equalizer-Effects-Pedals.gc',
   chorusFlangerAndPhaser: '/Used/Chorus-Flanger-Phaser-Effects-Pedals.gc',
 
+  floorEffects: '/Used/Floor-Guitar-Effects.gc',
+
   guitarAmplifiers: '/Used/Guitar-Amplifiers.gc',
-  GuitarAmplifierCombos: '/Used/Combo-Guitar-Amplifiers.gc',
+  guitarAmplifierCombos: '/Used/Combo-Guitar-Amplifiers.gc',
   guitarAmplifierHeads: '/Used/Guitar-Amplifier-Heads.gc',
+
+  directBoxes: '/Used/Direct-Boxes.gc',
+
+  synthesizers: '/Used/Synthesizers-Sound-Modules.gc',
 })
